cf68b828029f12199b738e7414724d9c26bc064f
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-11-22 09:24:37 +0100

    test: add test class for person

--- TestCarfleet/TestDriver.cs
+++ TestCarfleet/TestDriver.cs
@@ -6,18 +6,11 @@ namespace Carfleet
     public class TestDriver
     {
         #region private attributes
-        private string _name = "Einstein";
-        private string _firstname = "Albert";
-        private string _phonenumber = "+41793456789";
-        private string _emailaddress = "ae@relativity.org";
-        private List<string> _languages = new List<string>();
-        private Driver _driver;
         #endregion private attributes
 
         [SetUp]
         public void Setup()
         {
-            _driver = new Driver(_name, _firstname, _phonenumber, _emailaddress, _languages);
         }
 
         [Test]
